3a556ab2790048e18b676c029d35e0f23e37ddaa
ptrk <p.mikulski@campus.tu-berlin.de>
2017-06-21 19:10:10 +0200

    feature: pedestrian crossing traffic_signals but no highway traffic_signals found

--- src/main/java/org/openstreetmap/josm/plugins/lanes_and_traffic_signals_validation/SignalsTest.java
+++ src/main/java/org/openstreetmap/josm/plugins/lanes_and_traffic_signals_validation/SignalsTest.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.function.Predicate;
+
 
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.Way;
@@ -15,6 +17,9 @@ import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.validation.Test;
 import org.openstreetmap.josm.data.validation.TestError;
 import org.openstreetmap.josm.data.validation.Severity;
+
+import org.openstreetmap.josm.tools.Pair;
+
 
 
 public class SignalsTest extends Test{
@@ -49,25 +54,73 @@ public class SignalsTest extends Test{
     	return n.getParentWays().size() > 2;
     }
 
+    static int getNodeIdxAlongWay(Way w, Node n){
+    	if (n.getParentWays().contains(w)){
+			int nodesCount = w.getNodesCount();
+			for (int i = 0; i < nodesCount; i++){
+				if (w.getNode(i).equals(n)){
+					return i;
+				}
+			}
+		}
+		return 0;
+	}
+
+	private static Predicate<Node> hasTrafficSignals(){
+		return p -> p.hasTag("highway", "traffic_signals");
+	}
+
+	private static boolean preceededByTrafficSignal(Way w, Node n){
+		int nIdx = getNodeIdxAlongWay(w, n);
+		int end;
+		if (w.isOneway() > -1) {
+			end = 0;
+		}
+		else{
+			end = w.getNodesCount();
+		}
+		for (int i = nIdx; i > end;){
+			if (w.isOneway() > -1){
+				i--;
+			}
+			else{
+				i++;
+			}
+			if (w.getNode(i).hasTag("highway", "traffic_signals")){
+				return true;
+			}
+			if (SignalsTest.isJunction(w.getNode(i))){
+				return false;
+			}
+		}
+		return false;
+	}
+
     @Override
     public void visit(Node n) {
-    	if(n.isUsable() && !n.isOutsideDownloadArea()){
-    		if(n.hasTag("crossing","traffic_signals") && !n.hasTag("highway", "traffic_signals")){
-    			boolean ok = false;
-    			List<Way> parentWays = n.getParentWays();
-    			for(Way w : parentWays){
-    				Set<Node> neighbours = w.getNeighbours(n);
-    				for(Node neighbour : neighbours){
-    					if(SignalsTest.isJunction(neighbour)  || neighbour.hasTag("highway", "traffic_signals")){
-    						ok = true;	
-    					}
-    				}
-    			}
-    			if(!ok){
-    				this.junctions.add(n);
-    			}
-    		}
-    	}
+    	if(n.isUsable() && !n.isOutsideDownloadArea()) {
+			if (n.hasTag("crossing", "traffic_signals") && !n.hasTag("highway", "traffic_signals")) {
+				boolean ok = false;
+
+
+				List<Way> parentWays = n.getParentWays();
+				for (Way w : parentWays) {
+
+					if (w.hasTag("highway", "primary") ||
+							w.hasTag("highway", "secondary") ||
+							w.hasTag("highway", "teriary")) {
+
+						if (SignalsTest.preceededByTrafficSignal(w, n)) {
+							ok = true;
+						}
+
+					}
+				}
+				if (!ok) {
+					this.junctions.add(n);
+				}
+			}
+		}
     }
 
     @Override
@@ -81,7 +134,7 @@ public class SignalsTest extends Test{
 //		}
 		
 		for (Node junction : junctions){
-			String msg = "Junctions found in Endtest\n";
+			String msg = "Pedestrian crossing but no corresponding traffic signals found\n";
 			System.out.println(msg);
 			TestError error = TestError.builder(this, Severity.WARNING, 8888).message(msg).primitives(junction).highlight(junction).build();
 			errors.add(error);
